828604efa90c443a64feeda6a3cf3eef7127a33d
Amani Gharbi <amanigharbi9@gmail.com>
2025-04-07 17:28:04 +0200

    ajouter les pages login et register

--- chat-app/src/app/pages/login/login.component.ts
+++ chat-app/src/app/pages/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async login() {
+    try {
+      await this.auth.login(this.email, this.password);
+      this.router.navigateByUrl('/');
+    } catch (err: any) {
+      this.error = err.message;
+    }
+  }
 }
--- chat-app/src/app/pages/register/register.component.ts
+++ chat-app/src/app/pages/register/register.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async register() {
+    try {
+      await this.auth.register(this.email, this.password);
+      this.router.navigateByUrl('/');
+    } catch (err: any) {
+      this.error = err.message;
+    }
+  }
 }
